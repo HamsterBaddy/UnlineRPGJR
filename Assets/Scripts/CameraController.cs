023e4e3fb239b5ecef85b80fe4d89f3645ad3cbe
HamsterBaddy <71063396+HamsterBaddy@users.noreply.github.com>
2023-03-10 21:47:09 +0100

    more
    more

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0,0,-10);
+    //public float smoothTime = 0.3f;
+
+    //private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        player = NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+
+        //Vector3 targetPosition = player.position;
+        //targetPosition.z = transform.position.z;
+
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,19 @@ using Unity.Netcode;
 public class PlayerMovement : NetworkBehaviour
 {
     public Vector2 moveinput = new Vector2(0,0);
+    //public Vector2 oppositeForce = new Vector2(0,0);
+
+    public float speed = 3f;
+    public bool notSlide = false;
+    public Vector2 maxSpeed = new Vector2(3, 3);
+    //public float drag = 5f;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -21,15 +29,27 @@ public class PlayerMovement : NetworkBehaviour
             moveinput.y = Input.GetAxisRaw("Vertical");
             moveinput.x = Input.GetAxisRaw("Horizontal");
 
+            //oppositeForce = -rb.velocity.normalized * drag;
+
             setMoveInput_ServerRpc(moveinput, NetworkObjectId);
         }
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        if (notSlide)
+        {
+            if (moveinput.x == 0) rb.velocity = new Vector2(0, rb.velocity.y);
+            if (moveinput.y == 0) rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
 
-        rb.AddForce(moveinput);
+        rb.AddForce(moveinput * speed);
+
+        if (rb.velocity.x > maxSpeed.x) rb.velocity = new Vector2(maxSpeed.x, rb.velocity.y);
+        if (rb.velocity.y > maxSpeed.y) rb.velocity = new Vector2(rb.velocity.x, maxSpeed.y);
+        //nicht den M64 machen
+        if (rb.velocity.x < -maxSpeed.x) rb.velocity = new Vector2(-maxSpeed.x, rb.velocity.y);
+        if (rb.velocity.y < -maxSpeed.y) rb.velocity = new Vector2(rb.velocity.x, -maxSpeed.y);
     }
 
     [ServerRpc]
@@ -43,5 +63,8 @@ public class PlayerMovement : NetworkBehaviour
     {
         moveinput.x = value.x;
         moveinput.y = value.y;
+
+        //oppositeForce.x = opForce.x;
+        //oppositeForce.y = opForce.y;
     }
 }
